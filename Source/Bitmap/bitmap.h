0d1a5572ac786196d4522d2cd0718e99f896c44a
John Torborg <john.torborg@gmail.com>
2020-04-17 17:02:25 -0500

    Finish initial public method header

--- Source/Bitmap/bitmap.h
+++ Source/Bitmap/bitmap.h
@@ -3,7 +3,14 @@
 
 #include <stdio.h>
 
-//
-void write_array_to_bitmap(unsigned char *data);
+/**
+ * @brief Takes as input a 1D integer array (and image details) and writes to output.bmp
+ * 
+ * @param height The final image height
+ * 
+ * @param width The final image width
+ * 
+ */
+void write_array_to_bitmap(unsigned char *data, int height, int width);
 
 #endif

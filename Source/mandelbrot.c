ff910cc8358b5e89400b10f718d7f4779b1e10bc
Ethan Rose <errose28@gmail.com>
2020-04-19 19:21:01 -0400

    Add documentation

--- Source/mandelbrot.c
+++ Source/mandelbrot.c
@@ -8,9 +8,19 @@
 MB_Rgb _MB_rgb_color(const MB_Point *point);
 MB_Rgb _MB_hsv_to_rgb(int hue, double saturation, double value);
 MB_Rgb _MB_hsv_color(const MB_Point *point);
-double _MB_normalized_color(const MB_Point *point);
+double _MB_normalized_iterations(const MB_Point *point);
 
-MB_Point MB_is_mandelbrot(long double c_real, long double c_img, int iterations) {
+/**
+ * @brief Performs @p iterations number of mandelbrot set iterations on the imaginary number c given by @p c_real and @c
+ *      c_complex, and returns the resulting information.
+ * 
+ * @param c_real The real part of the number c to calculate Mandelbrot set information for.
+ * @param c_img The imaginary part of the number c to calculate Mandelbrot set information for.
+ * @param iterations The number of Mandelbrot set iterations to perform on c.
+ *
+ * @return An @c MB_Point instance containing information about the resulting iterations.
+ */
+MB_Point MB_iterate_mandelbrot(long double c_real, long double c_img, int iterations) {
     MB_Complex c = c_real + c_img * I;
     MB_Complex z = 0;
     MB_Complex z_next = 0;
@@ -30,7 +40,7 @@ MB_Point MB_is_mandelbrot(long double c_real, long double c_img, int iterations)
     }
 
     MB_Point info;
-    info.final_z = z;
+    info.z_final = z;
     info.iters_performed = iters_performed;
     info.max_iters = iterations;
     info.c = c;
@@ -38,6 +48,14 @@ MB_Point MB_is_mandelbrot(long double c_real, long double c_img, int iterations)
     return info;
 }
 
+/**
+ * @brief Returns an RGB color representation of @p point, using the specified conversion method to generate the color.
+ * 
+ * @param point The point whose Mandelbrot set information will be converted into a color.
+ * @param conversion The method to generate the color.
+ *
+ * @return An RGB color representation of @p point using its Mandelbrot set information.
+ */
 MB_Rgb MB_color_of(const MB_Point *point, MB_ColorMap conversion) {
     MB_Rgb color;
 
@@ -51,14 +69,19 @@ MB_Rgb MB_color_of(const MB_Point *point, MB_ColorMap conversion) {
     return color;
 }
 
-/*
-One possible way to map escape iterations to color is to scale the values and treat the first third as "x 0 0", the
-next third as "255 x 0" and the final third as "255 255 x". This will produce a gradient from black to red to yellow
-to white.
-*/
+/**
+ * @brief Generates an RGB color directly from an @c MB_Point.
+ *
+ * As the number of performed iterations on a point increases, this should produce a color gradient from white to
+ * yellow to red to black.
+ *
+ * @param point The Mandelbrot set information for a point that will be used to calculate its color.
+ *
+ * @return An RGB representation of @p point.
+ */
 MB_Rgb _MB_rgb_color(const MB_Point *point) {
     // Produces number on range (0, 3).
-    double color_fraction = _MB_normalized_color(point) * 3;
+    double color_fraction = _MB_normalized_iterations(point) * 3;
 
     MB_Rgb color;
 
@@ -82,34 +105,19 @@ MB_Rgb _MB_rgb_color(const MB_Point *point) {
     return color;
 }
 
-/*
-HSV colors:
-HUE
-    Red falls between 0 and 60 degrees.
-    Yellow falls between 61 and 120 degrees.
-    Green falls between 121-180 degrees.
-    Cyan falls between 181-240 degrees.
-    Blue falls between 241-300 degrees.
-    Magenta falls between 301-359 degrees.
-
-SATURATION
-Saturation describes the amount of gray in a particular color, from 0 to 100 percent (or 0 to 1).
-0 is grey, 1 is a primary color.
-
-VALUE (OR BRIGHTNESS)
-Value works in conjunction with saturation and describes the brightness or intensity of the color, from 0-100 percent
-(or 0 to 1).
-0 is black, 1 is brightest.
-
-// Compute the ratio of possible iterations before it diverged, and take that percent of the hue value.
-hue = (mu // MAX_ITER) * 360
-// No grey.
-saturation = 1
-// Black if in set, full brightness if not.
-value = 1 if m < MAX_ITER else 0
-*/
+/**
+ * @brief Generates an HSV color from @p point, and then converts it to an RGB color.
+ *
+ * Sets the hue to be a percentage of its maximum possible value, determined by the normalized iteration count.
+ * Sets the saturation as always one.
+ * Sets the value as 1 if @p point diverged, 0 otherwise.
+ *
+ * @param point The Mandelbrot set information for a point that will be used to calculate its color.
+ *
+ * @return An RGB representation of @p point converted from an HSV value.
+ */
 MB_Rgb _MB_hsv_color(const MB_Point *point) {
-    double color_percent = _MB_normalized_color(point);
+    double color_percent = _MB_normalized_iterations(point);
 
     int hue = floor(color_percent * 360);
     int saturation = 1;
@@ -124,8 +132,15 @@ MB_Rgb _MB_hsv_color(const MB_Point *point) {
     return _MB_hsv_to_rgb(hue, saturation, value);
 }
 
-// hue on [0, 359]
-// others on [0, 1]
+/**
+ * @brief Converts an HSV color to an RGB color.
+ * 
+ * @param hue The hue parameter of the color to convert, on the range [0, 359].
+ * @param saturation The saturation parameter of the color to convert, on the range [0, 1].
+ * @param value The value (or brightness) parameter of the color to convert, on the range [0, 1].
+ *
+ * @return An RGB color representation that is identical to the provided HSV color representation.
+ */
 MB_Rgb _MB_hsv_to_rgb(int hue, double saturation, double value) {
     double c = value * saturation;
     double x = c * (1 - abs(hue / 60 % 2 - 1));
@@ -141,31 +156,37 @@ MB_Rgb _MB_hsv_to_rgb(int hue, double saturation, double value) {
     int norm_hue = hue / 60;
 
     if (norm_hue == 0) {
+        // Red hue.
         color.red = c1;
         color.green = c2;
         color.blue = c3;
     }
     else if (norm_hue == 1) {
+        // Yellow hue.
         color.red = c2;
         color.green = c1;
         color.blue = c3;
     }
     else if (norm_hue == 2) {
+        // Green hue.
         color.red = c3;
         color.green = c1;
         color.blue = c2;
     }
     else if (norm_hue == 3) {
+        // Cyan hue.
         color.red = c3;
         color.green = c2;
         color.blue = c1;
     }
     else if (norm_hue == 4) {
+        // Blue hue.
         color.red = c2;
         color.green = c3;
         color.blue = c1;
     }
     else if (norm_hue == 5) {
+        // Magenta hue.
         color.red = c1;
         color.green = c3;
         color.blue = c2;
@@ -174,11 +195,20 @@ MB_Rgb _MB_hsv_to_rgb(int hue, double saturation, double value) {
     return color;
 }
 
-// Produces numbers on range (0, 1).
-// Higher iterations performed before escaping gives higher number.
-double _MB_normalized_color(const MB_Point *point) {
+/**
+ * @brief Computes the noormalized iteration count of @p point.
+ *
+ * Given the number of iterations performed and maximum possible iterations that could have been performed on @p point,
+ * generates a value on the range (0, 1), where a higher number means more iterations were performed before the point
+ * diverged.
+ *
+ * @param point The point whose Mandelbrot iteration information will be used in the computation.
+ *
+ * @return A value on the range (0, 1).
+ */
+double _MB_normalized_iterations(const MB_Point *point) {
     // Smooth is on (0, max_iter).
-    int smooth = point->iters_performed + 1 - logl(logl(cabsl(point->final_z))) / logl(2);
+    int smooth = point->iters_performed + 1 - logl(logl(cabsl(point->z_final))) / logl(2);
     // Normalize smooth to be on (0, 1).
     return smooth / point->max_iters;
 }

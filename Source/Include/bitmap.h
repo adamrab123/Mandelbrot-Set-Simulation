83f14b396bf71f15787c713cec82d638bf1233d2
John Torborg <john.torborg@gmail.com>
2020-04-22 23:09:47 -0500

    Fix small bugs, finish docs and add test

--- Source/Include/bitmap.h
+++ Source/Include/bitmap.h
@@ -1,64 +1,74 @@
 #ifndef BITMAP_H
 #define BITMAP_H
 
-#include <stdio.h>
 #include <mpi.h>
+#include <stdio.h>
 
 /**
  * @brief Represents an RGB color with red, green, and blue integer fields on [0, 255].
  */
 typedef struct {
-    unsigned char red;
-    unsigned char green;
     unsigned char blue;
+    unsigned char green;
+    unsigned char red;
 } Rgb;
 
+/**
+ * @brief Bitmap object containing file pointers and useful info 
+ */
 typedef struct {
-    // Public.
+    // Public
     char *image_file_name;
     FILE *serial_file;
     MPI_File *parallel_file;
     int height;
     int width;
 
-    // Private.
+    // Private
     int _padding_size;
 } Bitmap;
 
+/**
+ * @brief Designates Bitmap as setup for either a @c SERIAL or @c PARALLEL computation environment
+ */
 enum FileType { SERIAL, PARALLEL };
 typedef enum FileType FileType;
 
 /**
- * @brief Initialize the .bmp output file with a given height and width
+ * @brief Initialize the Btimap object and .bmp output file
  * 
- * @param height The image height in pixels
- * 
- * @param width The image width in pixels
- * 
+ * @param width Image width
+ * @param height Image height
+ * @param image_file_name Output file name
+ * @param file_type Designates Bitmap as setup for either a @c SERIAL or @c PARALLEL computation environment
+ * @return @c Bitmap* The Bitmap object to be output
  */
 Bitmap *Bitmap_init(int width, int height, const char *image_file_name, FileType file_type);
 
+/**
+ * @brief Bitmap destructor
+ * 
+ * @param self Bitmap object to be removed from memory
+ */
 void Bitmap_free(Bitmap *self);
 
 /**
- * @brief Writes passed pixel to the output file using sequential C methods
+ * @brief Writes passed pixel to the output file using serial C methods
  * 
- * @param pixel Array of size 3 containing values for each color
- * 
- * @param y The distance on the y-axis that the input pixel sits on the image
- * 
- * @param x The distance on the x-axis that the input pixel sits on the image
+ * @param self Bitmap object
+ * @param pixel Rgb enum containing color data
+ * @param x Pixel 'X' coordinate (offset for image plane)
+ * @param y Pixel 'Y' coordinate (offest for image plane)
  */
-void Bitmap_write_pixel_sequential(Bitmap *self, Rgb pixel, int x, int y);
+void Bitmap_write_pixel_serial(Bitmap *self, Rgb pixel, int x, int y);
 
 /**
  * @brief Writes passed pixel to the output file using parallel MPI methods
  * 
- * @param pixel Array of size 3 containing values for each color
- * 
- * @param y The distance on the y-axis that the input pixel sits on the image
- * 
- * @param x The distance on the x-axis that the input pixel sits on the image
+ * @param self Bitmap object
+ * @param pixel Rgb enum containing color data
+ * @param x Pixel 'X' coordinate (offset for image plane)
+ * @param y Pixel 'Y' coordinate (offest for image plane)
  */
 void Bitmap_write_pixel_parallel(Bitmap *self, Rgb pixel, int x, int y);
 

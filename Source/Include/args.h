062dd554e809445942df19c02614c7803e356e4a
Ethan Rose <errose28@gmail.com>
2020-04-30 15:12:06 -0400

    Remove mpfr from args files

--- Source/Include/args.h
+++ Source/Include/args.h
@@ -1,23 +1,17 @@
 #ifndef ARGS_H
 #define ARGS_H
 
-#include <gmp.h>
-#include <mpfr.h>
-#include <mpc.h>
-
 /**
  * @brief Contains information about the optional command 
  *      line parameters passed into our simulation
  */
 typedef struct {
-    mpfr_t x_min;
-    mpfr_t x_max;
-    mpfr_t y_min;
-    mpfr_t y_max;
-    mpfr_t step_size;
-    mpfr_prec_t prec;
-    mpfr_rnd_t rnd;
-    long long iterations;
+    double x_min;
+    double x_max;
+    double y_min;
+    double y_max;
+    double step_size;
+    long iterations;
     char *output_file;
     int block_size;
 } Args;
@@ -33,6 +27,6 @@ void Args_bitmap_dims(const Args *self, long *width, long *height);
 #ifdef __CUDACC__
 __host__ __device__
 #endif
-void Args_bitmap_to_complex(const Args *self, int x, int y, mpc_ptr c);
+void Args_bitmap_to_complex(const Args *self, int x, int y, double *c_real, double *c_imag);
 
 #endif // ARGS_H

9947b02dab3b835b27d54afd65926f0890b21c92
Ethan Rose <errose28@gmail.com>
2020-05-03 18:18:30 -0400

    Heavily remodel testing bash script

--- Test/run-tests.sh
+++ Test/run-tests.sh
@@ -1,140 +1,72 @@
-#!/bin/sh
+#!/bin/bash
 
-pwd
+function run_tests() {
+    step_size="$1"
+    chunk_size="$2"
+    num_ranks="$3"
+    block_size="$4"
+    output_dir="$5"
 
-# remove log files
-rm log_*
-rm -rf images
-mkdir images
-rm -rf output
+    iterations=100
+    num_gpus=1
 
-# --------- STRONG SCALING 1 ---------
+    ranks_per_node="$num_ranks"
+    num_nodes=1
 
-echo "Starting Strong Scaling 1"
+    if [ "$num_ranks" -eq 64 ]
+    then
+        ranks_per_node=32
+        num_nodes=2
+    fi
 
+    time_dir="$output_dir/${step_size}_${iterations}_${num_ranks}_${block_size}"
+
+    mkdir -p "$time_dir"
+
+    slurm_script="$(dirname "$0")/slurmTest.sh"
+
+    sbatch \
+        --nodes="$num_nodes" \
+        --ntasks-per-node="$ranks_per_node" \
+        --gres=gpu:"$num_gpus" \
+        --block-size="$block_size" \
+        --step-size="$step_size" \
+        --iterations="$iterations" \
+        --time-dir="$time_dir" \
+        --output="$time_dir/"log_%j.out
+        "$slurm_script" -c"$chunk_size"
+}
+
+# BEGIN SCRIPT EXECUTION
+
+output_dir=Output
+
+# Clean up after previous tests if necessary.
+# Prompt user to make sure data is not lost.
+[ -d "$output"] && rm -rI "$output_dir"
+
+# Step sizes used for weak scaling tests only.
+step_sizes=(0.032 0.016 0.008 0.004 0.002 0.001 0.0005)
 num_ranks_total=(1 2 4 8 16 32 64)
-block_size=(16 32 64 128 256 512 1024)
+chunk_sizes=(64 32 16 8 2 4 1)
+block_sizes=(16 32 64 128 256 512 1024)
 
-for i in ${!num_ranks_total[@]};
+# Index i is the same for all parallel arrays above.
+for i in "${!num_ranks_total[@]}";
 do
-	num_rank=${num_ranks_total[$i]}
-  	block=${block_size[$i]}
-  	
-  	num_gpus=1
+    chunks="${chunk_sizes[$i]}"
+    num_ranks="${num_ranks_total[$i]}"
+    block_size="${block_sizes[$i]}"
 
-  	num_nodes=1
-	ranks_per_node=$num_rank
+    # Strong scaling test 1.
+    step_size=0.001
+    run_tests "$step_size" "$chunks" "$num_ranks" "$block_sizes" "$output_dir"
 
-	if [ $num_ranks_total -eq 64 ]
-	then
-	    ranks_per_node=32
-	    num_nodes=2
-	fi
+    # Strong scaling test 2.
+    step_size=0.0001
+    run_tests "$step_size" "$chunks" "$num_ranks" "$block_sizes" "$output_dir"
 
-	
-	# creating the directory name
-	step_size=0.001
-	iterations=100
-	file=${step_size}_${iterations}_${num_rank}_${block}
-	time_dir="output/strong1/$file/"
-
-	# echo $time_dir
-
-	mkdir -p "output"
-	mkdir -p "output/strong1"
-	mkdir -p $time_dir
-	
-
-	# echo $ranks_per_node
-
-  	sbatch --nodes=$num_nodes --ntasks-per-node=$ranks_per_node --gres=gpu:$num_gpus "sim/slurmTest.sh" \
-  		--block-size=$block_size --step-size=$step_size --iterations=$iterations --time-dir=$time_dir --output-file="images/$file.bmp"
-done
-
-# --------- STRONG SCALING 2 ---------
-
-echo "Starting Strong Scaling 1"
-
-num_ranks_total=(1 2 4 8 16 32 64)
-block_size=(16 32 64 128 256 512 1024)
-
-for i in ${!num_ranks_total[@]};
-do
-	num_rank=${num_ranks_total[$i]}
-  	block=${block_size[$i]}
-  	
-  	num_gpus=1
-
-  	num_nodes=1
-	ranks_per_node=$num_rank
-
-	if [ $num_ranks_total -eq 64 ]
-	then
-	    ranks_per_node=32
-	    num_nodes=2
-	fi
-
-	
-	# creating the directory name
-	step_size=0.0001
-	iterations=100
-	file=${step_size}_${iterations}_${num_rank}_${block}
-	time_dir="output/strong2/$file/"
-
-	# echo $time_dir
-
-	mkdir -p "output"
-	mkdir -p "output/strong2"
-	mkdir -p $time_dir
-	
-
-	# echo $ranks_per_node
-
-  	sbatch --nodes=$num_nodes --ntasks-per-node=$ranks_per_node --gres=gpu:$num_gpus slurmTest.sh \
-  		--block-size=$block_size --step-size=$step_size --iterations=$iterations --time-dir=$time_dir --output-file="images/$file.bmp"
-done
-
-# --------- WEAK SCALING 2 ---------
-
-echo "Starting Weak Scaling 1"
-
-num_ranks_total=(1 2 4 8 16 32 64)
-block_size=(16 32 64 128 256 512 1024)
-step_size=(0.032 0.016 0.008 0.004 0.002 0.001 0.0005)
-
-for i in ${!num_ranks_total[@]};
-do
-	num_rank=${num_ranks_total[$i]}
-  	block=${block_size[$i]}
-  	step_size=${step_size[$i]}
-  	
-  	num_gpus=1
-
-  	num_nodes=1
-	ranks_per_node=$num_rank
-
-	if [ $num_ranks_total -eq 64 ]
-	then
-	    ranks_per_node=32
-	    num_nodes=2
-	fi
-
-	
-	# creating the directory name
-	
-	iterations=100
-	file=${step_size}_${iterations}_${num_rank}_${block}
-	time_dir="output/weak/$file/"
-
-	# echo $time_dir
-
-	mkdir -p "output"
-	mkdir -p "output/weak"
-	mkdir -p $time_dir
-	
-
-	# echo $ranks_per_node
-
-  	sbatch --nodes=$num_nodes --ntasks-per-node=$ranks_per_node --gres=gpu:$num_gpus slurmTest.sh \
-  		--block-size=$block_size --step-size=$step_size --iterations=$iterations --time-dir=$time_dir --output-file="images/$file.bmp"
+    # Weak scaling test.
+    step_size="${step_sizes[$i]}"
+    run_tests "$step_size" "$chunks" "$num_ranks" "$block_sizes" "$output_dir"
 done
--- Test/slurmTest.sh
+++ Test/slurmTest.sh
@@ -1,23 +1,19 @@
 #!/bin/bash -x
-#SBATCH --output=log_%j.out
 #SBATCH --time=30
 #SBATCH --partition=dcs
-# First argument to script is the block size in bytes.
-# second argument to script is the malloc type (host or device).
-
 
 if [ "$SLURM_NPROCS" ]
 then
-	if [ ! "$SLURM_NTASKS_PER_NODE" ]
-	then
-	SLURM_NTASKS_PER_NODE=1
-	fi
-	SLURM_NPROCS=‘expr $SLURM_JOB_NUM_NODES \* $SLURM_NTASKS_PER_NODE‘
+    if [ ! "$SLURM_NTASKS_PER_NODE" ]
+    then
+    SLURM_NTASKS_PER_NODE=1
+    fi
+    SLURM_NPROCS=‘expr $SLURM_JOB_NUM_NODES \* $SLURM_NTASKS_PER_NODE‘
 else
-	if [ ! "$SLURM_NTASKS_PER_NODE" ]
-	then
-	SLURM_NTASKS_PER_NODE=‘expr $SLURM_NPROCS / $SLURM_JOB_NUM_NODES‘
-	fi
+    if [ ! "$SLURM_NTASKS_PER_NODE" ]
+    then
+    SLURM_NTASKS_PER_NODE=‘expr $SLURM_NPROCS / $SLURM_JOB_NUM_NODES‘
+    fi
 fi
 
 srun hostname -s | sort -u > /tmp/hosts.$SLURM_JOB_ID
@@ -28,7 +24,6 @@ module load gcc/7.4.0/1
 module load spectrum-mpi
 module load cuda
 
-# Args are block_size, malloc type (host or device)
-time mpirun -hostfile /tmp/hosts.$SLURM_JOB_ID -np $SLURM_NPROCS "$HOME/scratch/Mandelbrot-Set-Simulation/Build/mandelbrot" $@
+time mpirun -hostfile /tmp/hosts.$SLURM_JOB_ID -np $SLURM_NPROCS "$HOME/scratch/Mandelbrot-Set-Simulation/Build/mandelbrot" -d "$@"
 
 rm /tmp/hosts.$SLURM_JOB_ID
